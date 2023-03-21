b38a17fc66fd42f92bd002c3874aab7b207c63db
Aayush Kukreja <113628608+Aayush6377@users.noreply.github.com>
2023-03-21 16:17:41 +0530

    Update Exercise4.c

--- Exercise Questions/Exercise4.c
+++ Exercise Questions/Exercise4.c
@@ -1,4 +1,4 @@
-//Write a C program to check whether a number is divisible by 5 or 11
+//Write a C program to check whether a number is divisible by 5 and 11 or not
 #include <stdio.h>
 int main(){
     int a;
@@ -7,8 +7,14 @@ int main(){
     if (a%5==0 && a%11==0){
         printf("%d is divisible by 5 and 11.",a);
     }
+    else if (a%5==0){
+        printf("%d is divisible by 5 but not 11.",a);
+    }
+    else if (a%11==0){
+        printf("%d is divisible by 11 but not 5.",a);
+    }
     else {
-        printf("%d is not divisible by 5 and 11.",a);
+        printf("%d is neither divisible 5 nor 11",a);
     }
     return 0;
 }
