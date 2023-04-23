c41a804c391615a062d8517a45c37cf66404286f
Aayush Kukreja <113628608+Aayush6377@users.noreply.github.com>
2023-04-23 15:50:33 +0530

    Update Question_5.c

--- Assignment 2/Question_5.c
+++ Assignment 2/Question_5.c
@@ -1,4 +1,4 @@
-/*Write a C program to input any character and check whether it is alphabet, digit or special character.*/
+/*Write a C program to input any character and check whether it is alphabet, digit or special character.*/
 #include <stdio.h>
 int main(){
     char c;
