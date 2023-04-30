6ff553e8e6b88b13962dca41e788e9687d9074a8
Aayush Kukreja <113628608+Aayush6377@users.noreply.github.com>
2023-05-01 00:09:45 +0530

    Update Question_35.c

--- PPS lab practical question 1-50/Question_35.c
+++ PPS lab practical question 1-50/Question_35.c
@@ -1,1 +1,18 @@
-/*WAP in C to count the no. of characters and no. of words of a string without using library functions*/#include <stdio.h>int main(){    char arr[200];    printf("Enter a string: ");    gets(arr);    int chr=0,word=1;    for (int i=0; arr[i]; i++){        chr++;        if (arr[i]==' ' && arr[i+1]!=' '){            word++;        }    }    printf("Number of characters: %d\n",chr);    printf("Number of words: %d",word);    return 0;}
+//WAP in C to count the no. of characters and no. of words of a string without using library functions
+#include <stdio.h>
+
+int main(){
+    char arr[200];
+    printf("Enter a string: ");
+    gets(arr);
+    int chr=0,word=1;
+    for (int i=0; arr[i]; i++){
+        chr++;
+        if (arr[i]==' ' && arr[i+1]!=' '){
+            word++;
+        }
+    }
+    printf("Number of characters: %d\n",chr);
+    printf("Number of words: %d",word);
+    return 0;
+}
